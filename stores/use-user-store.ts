aa2539d66aea2d2ca3d099b072def9cb9542eab5
uzlitefas <uzlitefas00@gmail.com>
2026-09-08 21:59:58 +0500

    use user store added

---
+++ stores/use-user-store.ts
@@ -0,0 +1,34 @@
+"use client"
+
+import { create } from "zustand"
+
+import type { User } from "@/service/user.service"
+
+type UserState = {
+  user: User | null
+  isLoading: boolean
+  error: string | null
+}
+
+type UserActions = {
+  setUser: (user: User) => void
+  clearUser: () => void
+  setLoading: (isLoading: boolean) => void
+  setError: (error: string | null) => void
+  clearError: () => void
+}
+
+const initialState: UserState = {
+  user: null,
+  isLoading: false,
+  error: null,
+}
+
+export const useUserStore = create<UserState & UserActions>()((set) => ({
+  ...initialState,
+  setUser: (user) => set({ user, isLoading: false, error: null }),
+  clearUser: () => set(initialState),
+  setLoading: (isLoading) => set({ isLoading }),
+  setError: (error) => set({ error }),
+  clearError: () => set({ error: null }),
+}))
